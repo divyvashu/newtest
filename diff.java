3630c5d717121ee0e7312e511c7955746f9c54e0
Divyanshu Sharma <divy15july@gmail.com>
2023-09-04 20:02:50 +0530

    commit 2 new file

---
+++ diff.java
@@ -0,0 +1,12 @@
+public class diff {
+    
+        public static void main(String args[]){
+    
+        
+        int a=5;
+        int b=6;
+        System.out.println("The sum is " + (a-b));
+        }
+    }
+    
+
