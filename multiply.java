664b09469fad4785c0835ad4a5fa0f8207091c7f
Divyanshu Sharma <divy15july@gmail.com>
2023-09-04 22:32:53 +0530

    added multiply

---
+++ multiply.java
@@ -0,0 +1,11 @@
+public class multiply {
+    public static void main(String args[]){
+    
+        
+        int a=5;
+        int b=6;
+        System.out.println("The multiply is " + (a*b));
+        }
+    }
+    
+
