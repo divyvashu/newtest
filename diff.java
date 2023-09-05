01e12a99b73fa2bcbb5525f262edaaf447658a20
Divyanshu Sharma <divy15july@gmail.com>
2023-09-05 07:42:52 +0530

    change in diff

--- diff.java
+++ diff.java
@@ -3,7 +3,7 @@ public class diff {
         public static void main(String args[]){
     
         
-        int a=5;
+        int a=6;
         int b=6;
         System.out.println("The sum is " + (a-b));
         }
